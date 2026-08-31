5b8f61e2e26f0f991449c02a67ba5146601ea464
Elmira Hanani <honey@elmira.local>
2026-08-31 11:09:46 +0700

    adding dummy data

--- lib/models/promo_banner.dart
+++ lib/models/promo_banner.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 class PromoBanner {
   //properties
   final String title;
-  final String subTitle;
+  final String subtitle;
   final String imageUrl;
   final List<Color> gradientColors;
 
-  PromoBanner({required this.title, required this.subTitle, required this.imageUrl, required this.gradientColors});
+  PromoBanner({required this.title, required this.subtitle, required this.imageUrl, required this.gradientColors});
 }
